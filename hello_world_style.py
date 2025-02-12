a1eabb57b891cd5d4927d20a4cc96eda5c7c0856
Volodymyr Kryzhanovskyi <volodymyr.kryzhnovskyi@stu.ocsb.ca>
2025-02-12 18:11:01 +0000

    hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Created By: Volodymyr Kryzhanovskyi
+# Date: 02/12/2025
+# Write My program basically states a welcoming message and one quote.
+
+
+# These specific sequances are the ones which make the code to state my quotes.
+print("Hello, people.")
+print("Lion doesn't concern his opinion with sheep.")
